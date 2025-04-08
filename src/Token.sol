3420eb4f48f8d50e422058149546d18f8d9f05e1
sanjay-in <sanjaysridharan@outlook.com>
2025-04-08 22:56:52 +0530

    update token contract

--- src/Token.sol
+++ src/Token.sol
@@ -1,21 +1,18 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.27;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Token is ERC20 {
-    address payable public owner;
-    address public creator;
-
-    constructor(
-        address _creator,
-        string memory _name,
-        string memory _symbol,
-        uint256 _totalSupply
-    ) ERC20(_name, _symbol) {
-        owner = payable(msg.sender);
-        creator = _creator;
-
+contract Token is ERC20, Ownable {
+    constructor(string memory _name, string memory _symbol, uint256 _totalSupply)
+        ERC20(_name, _symbol)
+        Ownable(msg.sender)
+    {
         _mint(msg.sender, _totalSupply);
+    }
+
+    function mint(address _to, uint256 _amount) external onlyOwner {
+        _mint(_to, _amount);
     }
 }
