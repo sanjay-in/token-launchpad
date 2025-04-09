beb052545610dbc8c758725345a913a4bce0a329
sanjay-in <sanjaysridharan@outlook.com>
2025-04-09 23:41:11 +0530

    add deploy script

---
+++ script/DeployTokenLaunchpad.s.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.27;
+
+import {Script} from "forge-std/Script.sol";
+import {Token} from "../src/Token.sol";
+import {TokenLaunchPad} from "../src/TokenLaunchpad.sol";
+
+contract DeployTokenLaunchpad is Script {
+    function run() external returns (TokenLaunchPad) {
+        TokenLaunchPad tokenLaunchpad = new TokenLaunchPad(1 * 10 ** 15);
+        return tokenLaunchpad;
+    }
+}
