6fd149d32ed907ec759b79a240de203406f2b94b
sanjay-in <sanjaysridharan@outlook.com>
2025-04-12 23:43:42 +0530

    add link to card

--- client/src/app/components/CardList.jsx
+++ client/src/app/components/CardList.jsx
@@ -1,39 +1,12 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { Contract, ethers } from "ethers";
+import Link from "next/link";
 import Card from "./Card";
-import { CONTRACT_ADDRESS, ABI } from "../../../constants/constants";
 import Loading from "./Loading";
 import NotFound from "./NotFound";
 import ErrorRetry from "./ErrorRetry";
 
-export const CardList = () => {
-  const [tokenDetails, setTokenDetails] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
-
-  const getAllTokens = async () => {
-    setLoading(true);
-    try {
-      if (window.ethereum) {
-        const provider = new ethers.BrowserProvider(window.ethereum);
-        const signer = await provider.getSigner();
-        const tokenContract = new Contract(CONTRACT_ADDRESS, ABI, signer);
-        const tokens = await tokenContract.getTokens();
-        if (tokens && tokens.length) {
-          setTokenDetails(tokens);
-        }
-      }
-    } catch (error) {
-      console.log(error);
-      setError(true);
-    }
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    getAllTokens();
-  }, []);
+export const CardList = ({ loading, error, tokenDetails }) => {
   return (
     <div className="cardlist">
       {loading ? (
@@ -47,7 +20,12 @@ export const CardList = () => {
           <div className="trending">now trending</div>
           <div className="cardlist-container">
             {tokenDetails.map((token) => {
-              return <Card />;
+              const [tokenAddress, name, creator, image, description, sold, raised, isOpen] = token;
+              return (
+                <Link href={`/token-details?token=${tokenAddress}`}>
+                  <Card key={tokenAddress} name={name} description={description} creator={creator} />
+                </Link>
+              );
             })}
           </div>
         </div>
