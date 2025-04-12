a94bfd42c4de69b7f52e7439e79d2da4f718b75c
sanjay-in <sanjaysridharan@outlook.com>
2025-04-12 23:45:00 +0530

    add progress bar and fetched details

--- client/src/app/token-details/page.jsx
+++ client/src/app/token-details/page.jsx
@@ -1,33 +1,132 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
+import { Contract, ethers } from "ethers";
+import { ABI, CONTRACT_ADDRESS } from "../../../constants/constants";
 
 export default () => {
+  const params = useSearchParams();
+  const paramsTokenAddress = params.get("token");
+
+  const [token, setToken] = useState({
+    token: 0,
+    name: "",
+    creator: 0,
+    imageUrl: "",
+    description: "",
+    sold: 0,
+    raised: 0,
+    isOpen: null,
+  });
+
+  let fundingGoal;
+  let fundingRaisedPercentage = 0;
+  let totalSupplyPercentage;
+  let initialSupply;
+  let totalSupply;
+
+  const getTokenDetails = async () => {
+    try {
+      if (window.ethereum) {
+        const provider = new ethers.BrowserProvider(window.ethereum);
+        const signer = await provider.getSigner();
+        const tokenContract = new Contract(CONTRACT_ADDRESS, ABI, signer);
+        const fetchedToken = await tokenContract.getTokenDetails(paramsTokenAddress);
+        let tokenAddress, name, creator, imageUrl, description, sold, raised, isOpen;
+        console.log(fetchedToken);
+        if (fetchedToken && fetchedToken.length) {
+          [tokenAddress, name, creator, imageUrl, description, sold, raised, isOpen] = fetchedToken;
+          setToken({
+            token: tokenAddress,
+            name,
+            creator,
+            imageUrl,
+            description,
+            sold,
+            raised,
+            isOpen,
+          });
+          return 1;
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getFundingDetails = async () => {
+    try {
+      if (window.ethereum) {
+        const provider = new ethers.BrowserProvider(window.ethereum);
+        const signer = await provider.getSigner();
+        const tokenContract = new Contract(CONTRACT_ADDRESS, ABI, signer);
+        initialSupply = parseInt(await tokenContract.INIT_SUPPLY());
+        totalSupply = parseInt(await tokenContract.MAX_SUPPLY());
+        fundingGoal = parseInt(await tokenContract.TARGET());
+        return 1;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getDetails = async () => {
+    const getTokenDetailsRes = await getTokenDetails();
+    if (getTokenDetailsRes) {
+      console.log("token.name", token);
+    }
+    await getFundingDetails();
+    fundingRaisedPercentage = (token.raised / fundingGoal) * 100;
+
+    console.log("token.raised", token.raised);
+    console.log("fundingGoal", fundingGoal);
+    totalSupplyPercentage = ((parseFloat(totalSupply) - initialSupply) / ethers.formatUnits(totalSupply - initialSupply, "ether")) * 100;
+  };
+
+  useEffect(() => {
+    getDetails();
+  }, []);
+
   return (
     <div className="token-details">
       <div className="start-coin">[go back]</div>
       <div className="details-containers">
         <div className="details-container">
-          <div className="details-heading">Token details for Spongebob token</div>
-          <img className="details-image" src="" />
+          <div className="details-heading">Token details for {token.name} token</div>
+          <img className="details-image" src={token.imageUrl} />
           <div className="details-content">
-            <span className="details-content-heading">Creator Address:</span>
+            <span className="details-content-heading">
+              Creator Address: <span className="details-content-value">{token.creator}</span>
+            </span>
           </div>
           <div className="details-content">
-            <span className="details-content-heading">Token Address:</span>
+            <span className="details-content-heading">
+              Token Address: <span className="details-content-value">{token.token}</span>
+            </span>
           </div>
           <div className="details-content">
-            <span className="details-content-heading">Funding Raised:</span>
+            <span className="details-content-heading">
+              Funding Raised: <span className="details-content-value">{token.raised}</span>
+            </span>
           </div>
           <div className="details-content">
-            <span className="details-content-heading">Token Symbol:</span>
+            <span className="details-content-heading">
+              Token Symbol: <span className="details-content-value">{token.token}</span>
+            </span>
           </div>
           <div className="details-content">
-            <span className="details-content-heading">Description:</span>
+            <span className="details-content-heading">
+              Description: <span className="details-content-value">{token.description}</span>
+            </span>
           </div>
         </div>
         <div className="details-container">
           <div className="details-content">
             <span className="details-content-heading">Bonding Curve Progress:</span>
             0/24 ETH
+          </div>
+          <div className="progress-bar">
+            <div className="progress" style={{ width: `${fundingRaisedPercentage}%` }}></div>
           </div>
           <div className="details-content-bonding-curve-description">
             When the market cap reaches 24 ETH, all the liquidity from the bonding curve will be deposited into Uniswap, and the LP tokens will be
@@ -36,6 +135,9 @@ export default () => {
           <div className="details-content">
             <span className="details-content-heading">Remaining tokens available for sale:</span>
             800,000 / 800,000
+          </div>
+          <div className="progress-bar">
+            <div className="progress" style={{ width: `${fundingRaisedPercentage}%` }}></div>
           </div>
           <div className="buy-tokens-heading">Buy Tokens</div>
           <input type="number" className="token-quantity-field" placeholder="Enter token quantity" />
