b3cd4154d2eb6137950c14a8e1457e87c2e6060b
sanjay-in <sanjaysridharan@outlook.com>
2025-04-12 23:43:06 +0530

    update card with fetched values

--- client/src/app/components/Card.jsx
+++ client/src/app/components/Card.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import { trimAddress } from "../../../utils/common";
 
-export default () => {
+export default ({ name, description, creator }) => {
   return (
     <div className="card-elements">
       <img src="https://pexx.com/chaindebrief/wp-content/uploads/2023/05/image-20.png" className="card-img" />
       <div className="info-container">
-        <div className="createdby">created by A75bRjK 3h ago</div>
+        <div className="createdby">created by {trimAddress(creator, 0, 7)} 3h ago</div>
         <div className="market-cap">market cap</div>
-        <div className="title">Title</div>
-        <div className="description">Description</div>
+        <div className="title">Title: {name}</div>
+        <div className="description">Description: {description.substr(0, 50)}...</div>
       </div>
     </div>
   );
