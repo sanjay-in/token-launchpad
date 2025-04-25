cabc0254f16ad5d404cb86db5182fe0fedc8af62
sanjay-in <sanjaysridharan@outlook.com>
2025-04-25 19:47:28 +0530

    update token details page

--- client/src/app/token-details/page.jsx
+++ client/src/app/token-details/page.jsx
@@ -1,30 +1,44 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { useSearchParams } from "next/navigation";
-import { Contract, ethers } from "ethers";
+import { useSearchParams, useRouter } from "next/navigation";
+import { BrowserProvider, Contract, ethers } from "ethers";
 import { ABI, CONTRACT_ADDRESS } from "../../../constants/constants";
+import BuyConfirmationModal from "../components/BuyConfirmationModal";
+import Loading from "../components/Loading";
+import ImageNotAvailable from "../../../public/Image_not_available.png";
 
 export default () => {
   const params = useSearchParams();
+  const router = useRouter();
   const paramsTokenAddress = params.get("token");
 
   const [token, setToken] = useState({
     token: 0,
     name: "",
     creator: 0,
-    imageUrl: "",
+    imageUrl: ImageNotAvailable.src,
     description: "",
     sold: 0,
     raised: 0,
     isOpen: null,
   });
+  const [tokensToBuy, setTokensToBuy] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  const [isInvalidToken, setIsInvalidToken] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [isBuying, setIsBuying] = useState(false);
+  const [isConfirmationModalOpen, setIsConfirmationModalOpen] = useState(false);
+  const [isFirstRender, setIsFirstRender] = useState(true);
 
-  let fundingGoal;
-  let fundingRaised;
-  let fundingRaisedPercentage = 0;
-  let totalSupplyPercentage;
-  let initialSupply;
-  let totalSupply;
+  const [percentageValues, setPercentageValues] = useState({
+    fundingRaisedPercentage: 0,
+    totalSupplyPercentage: 0,
+  });
+  const [fundingValues, setFundingValues] = useState({
+    fundingGoal: 0,
+    initialSupply: 0,
+    totalSupply: 0,
+  });
 
   const getTokenDetails = async () => {
     try {
@@ -32,27 +46,29 @@ export default () => {
         const provider = new ethers.BrowserProvider(window.ethereum);
         const signer = await provider.getSigner();
         const tokenContract = new Contract(CONTRACT_ADDRESS, ABI, signer);
+
         const fetchedToken = await tokenContract.getTokenDetails(paramsTokenAddress);
         let tokenAddress, name, creator, imageUrl, description, sold, raised, isOpen;
-        console.log(fetchedToken);
         if (fetchedToken && fetchedToken.length) {
           [tokenAddress, name, creator, imageUrl, description, sold, raised, isOpen] = fetchedToken;
-          fundingRaised = parseInt(raised);
           setToken({
             token: tokenAddress,
             name,
             creator,
             imageUrl,
             description,
-            sold: parseInt(sold),
-            raised: parseInt(raised),
+            sold: Number(sold),
+            raised: Number(ethers.formatEther(raised)),
             isOpen,
           });
           return 1;
         }
       }
     } catch (error) {
-      console.log(error);
+      if (error.code == "INVALID_ARGUMENT" && error.argument == "address") {
+        setIsInvalidToken(true);
+      }
+      console.error(error);
     }
   };
 
@@ -62,84 +78,156 @@ export default () => {
         const provider = new ethers.BrowserProvider(window.ethereum);
         const signer = await provider.getSigner();
         const tokenContract = new Contract(CONTRACT_ADDRESS, ABI, signer);
-        initialSupply = parseInt(await tokenContract.INIT_SUPPLY());
-        totalSupply = parseInt(await tokenContract.MAX_SUPPLY());
-        fundingGoal = parseInt(await tokenContract.TARGET());
+
+        const fundingGoal = await tokenContract.TARGET();
+        const initialSupply = parseInt(await tokenContract.INIT_SUPPLY());
+        const maxSupply = parseInt(await tokenContract.MAX_SUPPLY());
+        const totalSupply = maxSupply - initialSupply;
+
+        setFundingValues({
+          fundingGoal: Number(ethers.formatEther(fundingGoal)),
+          initialSupply,
+          totalSupply,
+        });
         return 1;
       }
     } catch (error) {
-      console.log(error);
+      console.error(error);
     }
   };
 
   const getDetails = async () => {
     await getTokenDetails();
     await getFundingDetails();
-    fundingRaisedPercentage = (fundingRaised / fundingGoal) * 100;
-    totalSupplyPercentage = ((totalSupply - initialSupply) / ethers.formatUnits(totalSupply - initialSupply, "ether")) * 100;
+  };
+
+  const buyTokens = async () => {
+    setIsBuying(true);
+    try {
+      if (window.ethereum) {
+        const provider = new BrowserProvider(window.ethereum);
+        const signer = await provider.getSigner();
+        const tokenContract = new Contract(CONTRACT_ADDRESS, ABI, signer);
+        const currentSupply = parseInt(await tokenContract.getTotalSupply(token.token));
+        const costOfTokens = await tokenContract.calculateCost(currentSupply, tokensToBuy);
+        const buyTransactionReciept = await tokenContract.buy(token.token, tokensToBuy, { value: costOfTokens });
+        const buyTransactionResponse = await buyTransactionReciept.wait();
+        if (buyTransactionResponse.status == 1) {
+          setIsConfirmationModalOpen(true);
+        }
+      }
+    } catch (error) {
+      console.log(error);
+      setIsConfirmationModalOpen(true);
+      setIsError(true);
+    }
+    setIsBuying(false);
   };
 
   useEffect(() => {
-    getDetails();
-  }, []);
+    setIsLoading(true);
+
+    if (isFirstRender) {
+      getDetails();
+    }
+    setIsFirstRender(false);
+
+    const { fundingGoal, initialSupply, totalSupply } = fundingValues;
+    const fundingRaisedPercentage = (token.raised / fundingValues.fundingGoal) * 100;
+    const totalSupplyPercentage = (token.sold / totalSupply) * 100;
+    setPercentageValues({
+      fundingRaisedPercentage,
+      totalSupplyPercentage,
+    });
+
+    setIsLoading(false);
+  }, [fundingValues]);
 
   return (
     <div className="token-details">
-      <div className="start-coin">[go back]</div>
-      <div className="details-containers">
-        <div className="details-container">
-          <div className="details-heading">Token details for {token.name} token</div>
-          <img className="details-image" src={token.imageUrl} />
-          <div className="details-content">
-            <span className="details-content-heading">
-              Creator Address: <span className="details-content-value">{token.creator}</span>
-            </span>
-          </div>
-          <div className="details-content">
-            <span className="details-content-heading">
-              Token Address: <span className="details-content-value">{token.token}</span>
-            </span>
-          </div>
-          <div className="details-content">
-            <span className="details-content-heading">
-              Funding Raised: <span className="details-content-value">{token.raised}</span>
-            </span>
-          </div>
-          <div className="details-content">
-            <span className="details-content-heading">
-              Token Symbol: <span className="details-content-value">{token.token}</span>
-            </span>
-          </div>
-          <div className="details-content">
-            <span className="details-content-heading">
-              Description: <span className="details-content-value">{token.description}</span>
-            </span>
-          </div>
+      {isLoading ? (
+        <div className="loading-token-details">
+          <Loading text="Fetching token details..." />
         </div>
-        <div className="details-container">
-          <div className="details-content">
-            <span className="details-content-heading">Bonding Curve Progress:</span>
-            {token.sold}/24 ETH
+      ) : isInvalidToken ? (
+        <div className="loading-token-details">Invalid Token. Please check your token address.</div>
+      ) : (
+        <>
+          <div className="start-coin" onClick={() => router.back()}>
+            [go back]
           </div>
-          <div className="progress-bar">
-            <div className="progress" style={{ width: `${fundingRaisedPercentage}%` }}></div>
+          <div className="details-containers">
+            <div className="details-container">
+              <div className="details-heading">Token details for {token.name}</div>
+              <img className="details-image" src={token.imageUrl} />
+              <div className="details-content">
+                <span className="details-content-heading">
+                  Creator Address: <span className="details-content-value">{token.creator}</span>
+                </span>
+              </div>
+              <div className="details-content">
+                <span className="details-content-heading">
+                  Token Address: <span className="details-content-value">{token.token}</span>
+                </span>
+              </div>
+              <div className="details-content">
+                <span className="details-content-heading">
+                  Funding Raised: <span className="details-content-value">{token.raised} ETH</span>
+                </span>
+              </div>
+              <div className="details-content">
+                <span className="details-content-heading">
+                  Token Symbol: <span className="details-content-value">{token.token}</span>
+                </span>
+              </div>
+              <div className="details-content">
+                <span className="details-content-heading">
+                  Description: <span className="details-content-value">{token.description}</span>
+                </span>
+              </div>
+            </div>
+            <div className="details-container">
+              <div className="details-content">
+                <span className="details-content-heading">Bonding Curve Progress:</span>
+                {token.raised} / 24 ETH
+              </div>
+              <div className="progress-bar">
+                <div className="progress" style={{ width: `${percentageValues.fundingRaisedPercentage}%` }}></div>
+              </div>
+              <div className="details-content-bonding-curve-description">
+                When the market cap reaches 24 ETH, all the liquidity from the bonding curve will be deposited into Uniswap, and the LP tokens will be
+                burned. Progression increases as the price goes up.
+              </div>
+              <div className="details-content">
+                <span className="details-content-heading">Remaining tokens available for sale:</span>
+                {token.sold.toLocaleString()} / {fundingValues.totalSupply.toLocaleString()}
+              </div>
+              <div className="progress-bar">
+                <div className="progress" style={{ width: `${percentageValues.totalSupplyPercentage}%` }}></div>
+              </div>
+              <div className="buy-tokens-heading">Buy Tokens</div>
+              <input
+                type="number"
+                className="token-quantity-field"
+                placeholder="Enter token quantity"
+                value={tokensToBuy}
+                onChange={(e) => setTokensToBuy(e.target.value)}
+              />
+              <button className="purchase-button" onClick={buyTokens}>
+                Purchase
+              </button>
+            </div>
           </div>
-          <div className="details-content-bonding-curve-description">
-            When the market cap reaches 24 ETH, all the liquidity from the bonding curve will be deposited into Uniswap, and the LP tokens will be
-            burned. Progression increases as the price goes up.
-          </div>
-          <div className="details-content">
-            <span className="details-content-heading">Remaining tokens available for sale:</span>
-            800,000 / 800,000
-          </div>
-          <div className="progress-bar">
-            <div className="progress" style={{ width: `${fundingRaisedPercentage}%` }}></div>
-          </div>
-          <div className="buy-tokens-heading">Buy Tokens</div>
-          <input type="number" className="token-quantity-field" placeholder="Enter token quantity" />
-          <button className="purchase-button">Purchase</button>
-        </div>
-      </div>
+        </>
+      )}
+      {isConfirmationModalOpen ? (
+        <BuyConfirmationModal
+          type={isError ? "error" : "success"}
+          closeModal={setIsConfirmationModalOpen}
+          show={isConfirmationModalOpen}
+          amountOfTokens={tokensToBuy}
+        />
+      ) : null}
     </div>
   );
 };
